236db7854dcc4ae7b2f7abcdee0b52ae81527872
Sarah Bühler <97050915+SarahBuehler@users.noreply.github.com>
2022-06-27 10:53:33 +0200

    add metronome parts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Metronome from './Metronome';
+ 
+ReactDOM.render(<Metronome/>, document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/14145/
+// Metronom Aufgabe Modul 5
